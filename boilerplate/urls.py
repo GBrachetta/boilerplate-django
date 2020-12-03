bb9fd2c566db0d122f6eb279365613925a2182a4
Guillermo Brachetta <brachetta@me.com>
2020-12-04 00:30:14 +0100

    Added media context processor and media to urls in development

--- boilerplate/urls.py
+++ boilerplate/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -13,3 +14,7 @@ if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include(debug_toolbar.urls)),
     ]
+
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
