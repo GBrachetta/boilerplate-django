e1086a277aa5a72dad62cdbc8e2a1b06f5ca31dd
Guillermo Brachetta <brachetta@me.com>
2020-12-03 20:59:51 +0100

    Swapped names of static_root and staticfiles_dir

--- boilerplate/settings/components/common.py
+++ boilerplate/settings/components/common.py
@@ -71,8 +71,8 @@ USE_L10N = True
 USE_TZ = True
 
 # Static and Media
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
